34ad992182475cf848ecd0958344597b639154d1
Kushagra Malwania <kushagramalwania19@gnu.ac.in>
2022-11-11 01:29:46 +0530

    Added GraphQL and storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,18 @@ export type AmplifyDependentResourcesAttributes = {
             "AppClientIDWeb": "string",
             "AppClientID": "string"
         }
+    },
+    "api": {
+        "librarymanagementsys": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "file": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
 }
